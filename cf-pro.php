a61a6170ec94ea9ecda19bfc4291fda8ba9258d6
Josh Pollock <Josh@JoshPress.net>
2017-06-07 20:09:18 -0400

    v 0.1.0

--- cf-pro.php
+++ cf-pro.php
@@ -1,14 +1,14 @@
 <?php
 /**
  Plugin Name: Caldera Forms Pro Client
-Version: 0.0.1
+Version: 0.1.0
  */
 
 if( ! defined( 'CF_PRO_VER' ) ){
 	/**
 	 * Caldera Forms Pro Client Version
 	 */
-	define( 'CF_PRO_VER', '0.0.1' );
+	define( 'CF_PRO_VER', '0.1.0' );
 
 	include_once  dirname( __FILE__ ) . '/bootstrap-cf-pro.php';
 }
