7dd9c15a5b094710cc0e578b3279d6a8c0dc87ed
Josh Pollock <Josh@JoshPress.net>
2017-06-11 14:38:21 -0400

    don't load if PHP version sucks

--- cf-pro.php
+++ cf-pro.php
@@ -3,12 +3,24 @@
  Plugin Name: Caldera Forms Pro Client
 Version: 0.1.0
  */
+if ( ! version_compare( PHP_VERSION, '5.4.0', '>=' ) ) {
+	add_action( 'admin_notices', 'caldera_forms_pro_version_fail_warning' );
+	function caldera_forms_pro_version_fail_warning(){
+		$class = 'notice notice-error';
+		$message = __( 'Caldera Forms Pro could not be loaded because your PHP is out of date.', 'cf-pro' );
 
-if( ! defined( 'CF_PRO_VER' ) ){
-	/**
-	 * Caldera Forms Pro Client Version
-	 */
-	define( 'CF_PRO_VER', '0.1.0' );
+		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+	}
 
-	include_once  dirname( __FILE__ ) . '/bootstrap-cf-pro.php';
+
+}else{
+	if( ! defined( 'CF_PRO_VER' ) ){
+		/**
+		 * Caldera Forms Pro Client Version
+		 */
+		define( 'CF_PRO_VER', '0.1.0' );
+
+		include_once  dirname( __FILE__ ) . '/bootstrap-cf-pro.php';
+	}
+
 }
