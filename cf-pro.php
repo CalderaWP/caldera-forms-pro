25b81f1a51428a958e95c1fb797dcc7aa9b27c1e
Josh Pollock <jpollock412@gmail.com>
2017-09-26 16:03:28 -0400

    Check CF Core version #59

--- cf-pro.php
+++ cf-pro.php
@@ -6,7 +6,7 @@
  * Description: Client plugin for the Caldera Forms Pro app.
  * Author: Caldera Labs
  * Author URI: http://CalderaLabs.org
- * Version: 0.11.1
+ * Version: 0.11.2
  */
 
 
@@ -14,13 +14,21 @@
 if ( ! version_compare( PHP_VERSION, '5.6.0', '>=' ) ) {
 	add_action( 'admin_notices', 'caldera_forms_pro_version_fail_warning' );
 	function caldera_forms_pro_version_fail_warning(){
-		$class = 'notice notice-error';
+		$class   = 'notice notice-error';
 		$message = __( 'Caldera Forms Pro could not be loaded because your PHP is out of date. Caldera Forms Pro requires PHP 5.6 or later.', 'cf-pro' );
 
 		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 	}
 
 
+}elseif ( ! defined( 'CFCORE_VER' ) ||  ! version_compare( CFCORE_VER, '1.5.1', '>=' ) ){
+	add_action( 'admin_notices', 'caldera_forms_pro_cf_version_fail_warning' );
+	function caldera_forms_pro_cf_version_fail_warning(){
+		$class   = 'notice notice-error';
+		$message = __( 'Caldera Forms Pro could not be loaded because Caldera Forms is not installed or is out of date.', 'cf-pro' );
+
+		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+	}
 }else{
 	if( ! defined( 'CF_PRO_VER' ) ){
 
@@ -34,9 +42,10 @@ if ( ! version_compare( PHP_VERSION, '5.6.0', '>=' ) ) {
 		/**
 		 * Caldera Forms Pro Client Version
 		 */
-		define( 'CF_PRO_VER', '0.11.1' );
+		define( 'CF_PRO_VER', '0.11.2' );
 
 		include_once  dirname( __FILE__ ) . '/bootstrap-cf-pro.php';
+
 
 
 		register_activation_hook( __FILE__, 'caldera_forms_pro_activation_hook_callback' );
